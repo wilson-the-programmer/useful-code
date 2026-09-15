38b7624b26c49563cb4e35d67992ef653f8a525d
Wilson <wilsony175@gmail.com>
2026-09-14 21:06:27 -0400

    Update q_shell.cpp

--- q_shell.cpp
+++ q_shell.cpp
@@ -1,20 +1,63 @@
-
 #include <QApplication>
 #include <qtermwidget6/qtermwidget.h>
+#include <QRandomGenerator>
+#include <vector>
+
+QString randScheme()
+{
+    std::vector<QString> schemes = {
+        "BlackOnLightYellow",
+        "BlackOnRandomLight",
+        "BlackOnWhite",
+        "BreezeModified",
+        "DarkPastels",
+        "Falcon",
+        "GreenOnBlack",
+        "Linux",
+        "Nord",
+        "Solarized",
+        "SolarizedLight",
+        "Tango",
+        "Ubuntu",
+        "WhiteOnWhite"
+    };
+
+    int index = QRandomGenerator::global()->bounded(static_cast<int>(schemes.size()));
+
+    return schemes[index];
+}
+
+
+
+//QString my_scheme = randScheme();
+
+
+QString my_scheme = "Linux";
+
+
+//terminal->setColorScheme(my_scheme);
+
+
+
+
 
 int main(int argc, char *argv[])
 {
+
     QApplication app(argc, argv);
 
     QTermWidget terminal;
 
     terminal.setShellProgram("/bin/bash");
-    terminal.setColorScheme("DarkPastels");
-    terminal.setTerminalFont(QFont("Monospace", 9));
+    terminal.setColorScheme(my_scheme);
+    terminal.setTerminalFont(QFont("Monospace", 12));
+    //terminal.sendText("Terminal Scheme: " + my_scheme + "\n");
 
-    terminal.resize(200, 200);
+    terminal.resize(800, 650);
+
     terminal.show();
 
     return app.exec();
+
 }
 
